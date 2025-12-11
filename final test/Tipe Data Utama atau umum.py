89e6c4a097b31461d74b4bf0ebf24e7e1d6e5c50
nurmaulidina_sisfob <113230323+nurmaulidina@users.noreply.github.com>
2025-12-11 19:16:17 +0800

    Create Tipe Data Utama atau umum.py

---
+++ final test/Tipe Data Utama atau umum.py
@@ -0,0 +1,5 @@
+# tipe data yg sering dgunakan
+name = "nurmaulidina" # string
+umur = 19 # integer hanya berisi nilai angka 
+tb = 160.2 # float untuk bilangan desimal
+masihHidup = True # boolean , hnya berisi nilai salah dan benar
