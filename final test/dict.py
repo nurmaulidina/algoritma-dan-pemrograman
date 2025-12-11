e0c3e2cbc1f8dcaa8a3f7df24957f0db218fa264
nurmaulidina_sisfob <113230323+nurmaulidina@users.noreply.github.com>
2025-12-11 19:20:12 +0800

    Update dict.py

--- final test/dict.py
+++ final test/dict.py
@@ -1,1 +1,23 @@
-Dictionary
+# Contoh penggunaan dictionary Python
+# -----------------------------------
+
+mahasiswa = {
+    "nama": "nrmaulidina",
+    "nim": "D0425318",
+    "jurusan": "sisfo",
+    "nilai": 90 # hehe
+}
+
+print("Nama:", mahasiswa["nama"])
+print("NIM:", mahasiswa["nim"])
+
+# Menambah data
+mahasiswa["alamat"] = "pamboang"
+
+# Mengubah data
+mahasiswa["nilai"] = 95
+
+# Menghapus data
+del mahasiswa["jurusan"]
+
+print("Data mahasiswa:", mahasiswa)
