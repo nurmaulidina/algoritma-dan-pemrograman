949946aa70770d1525b8f7622d5032555c94cb26
nurmaulidina_sisfob <113230323+nurmaulidina@users.noreply.github.com>
2025-12-11 19:19:18 +0800

    Update Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimens.py

--- final test/Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimens.py
+++ final test/Tipe Data  array Satu Dimensi, Dua Dimansi, dan Tiga Dimens.py
@@ -1,1 +1,29 @@
 
+# Tipe Data Array 1D, 2D, dan 3D
+# ------------------------------
+
+# ARRAY 1 DIMENSI
+array1D = [10, 20, 30, 40]
+
+# ARRAY 2 DIMENSI (matriks)
+array2D = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# ARRAY 3 DIMENSI
+array3D = [
+    [
+        [1, 2],
+        [3, 4]
+    ],
+    [
+        [5, 6],
+        [7, 8]
+    ]
+]
+
+print("Array 1D:", array1D)
+print("Array 2D:", array2D)
+print("Array 3D:", array3D)
